3d280ed53b2bf7f6b6ddadb9eea03a993232e663
anidaniel <anidanielcorp@gmail.com>
2019-03-28 21:56:17 +0530

    alpha

---
+++ Test_project/DataTypes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Test_project
+{
+    public class DataTypes
+    {
+        public int x;
+        public int y;
+
+        public int summation(int x,int y)
+        {
+            return x+y;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        DataTypes obj1 = new DataTypes();
+        //DataTypes data;  //object creation
+        int sum = obj1.summation(5,2);
+        Console.WriteLine("Summation: {0}", sum);
+        Console.ReadLine();
+    }
+}
---
+++ Test_project/Struct.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_project
+{
+    //struct Anidaniel
+    //{
+    //    string name, addr;
+    //    int num;
+    //};
+
+    class Struct
+    {
+        struct Anidaniel
+        {
+            public string name, addr;       // keyword "public" is essential or it won't take the reference from the struct as it'll be protected
+            public int num;
+        };
+
+        static void Main(string[] args)
+        {
+
+            Anidaniel rabi;
+            rabi.name = "Rabiyudeen Jalaludeen";
+            rabi.addr = "Habibi from DUBAI";
+            rabi.num = 987654123;
+            Console.WriteLine("NAME: {0}\nADDRESS: {1}\nNUMBER: {2}\n ",rabi.name,rabi.addr,rabi.num);
+            //Console.WriteLine("WTF");
+            Console.ReadLine();
+        }
+    }
+}
